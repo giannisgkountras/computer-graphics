583726d680924088d1702c309b03bef42a239650
giannisgkountras <gkountrasioannis@gmail.com>
2024-03-26 02:44:36 +0200

    Fixed bug in interpolation when p2 was the leftmost or downmost point

--- Ergasia_1/functions.py
+++ Ergasia_1/functions.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 
+
 def vector_interp(p1, p2, V1, V2, coord, dim):
     """
     Function for computing vector corresponding to point p with linear interpolation
@@ -16,29 +17,73 @@ def vector_interp(p1, p2, V1, V2, coord, dim):
     Returns:
         array: The interpolated vector corresponding to p based on p1 and p2.
     """
-    
+
     if dim == 1:
+        # Check which point is more left and name its x coordinate x1
         if p1[0] < p2[0]:
             x1 = p1[0]
             x2 = p2[0]
         else:
+            # In the case of p2 beeing more left than p1 we name x1
+            # the x coordinate of p2 and swap the names of the vectors (V1 becomes V2 and the opposite)
             x1 = p2[0]
             x2 = p1[0]
+            V_temp = V1
+            V1 = V2
+            V2 = V_temp
 
+        # Calculate the distance between the points on the x axis
         width = x2 - x1
+
+        # Calculate what percentage of width is the distance on the
+        # x axis between p and the most left point
         percentage = (coord - x1) / width
+
+        # Calculate the final vector by doing a linear interpolation
+        # using the percentage and V1 and V2. Since the percentage is calculated based
+        # on the most left point, we use (1 - percentage) for V1
         V = np.add(np.multiply((1 - percentage), V1), np.multiply(percentage, V2))
 
     if dim == 2:
+        # Check which point is more down and name its y coordinate y1
         if p1[1] < p2[1]:
             y1 = p1[1]
             y2 = p2[1]
         else:
+            # In the case of p2 beeing more down than p1 we name y1
+            # the y coordinate of p2 and swap the names of the vectors (V1 becomes V2 and the opposite)
             y1 = p2[1]
             y2 = p1[1]
+            V_temp = V1
+            V1 = V2
+            V2 = V_temp
 
+        # Calculate the distance between the points on the y axis
         height = y2 - y1
+
+        # Calculate what percentage of height is the distance on the
+        # y axis between p and the most down point
         percentage = (coord - y1) / height
+
+        # Calculate the final vector by doing a linear interpolation
+        # using the percentage and V1 and V2. Since the percentage is calculated based
+        # on the most down point, we use (1 - percentage) for V1
         V = np.add(np.multiply((1 - percentage), V1), np.multiply(percentage, V2))
 
     return V
+
+
+def f_shading(img, vertices, vcolors):
+    """
+    This image gives triangles the vector mean of the 3 apex colors.
+
+    Args:
+        img (MxNx3 array): The image with previously existing triangles
+        vertices (3x2 array): Each row contains the 2D coordinates of an apex of a triangle
+        vcolors (3x3 array): Each row contains the color of the corresponding point in RGB [0,1]
+
+    Returns
+        MxNx3 array: The updated image with RGB for each pixel plus the old image (overlapping common pixels)
+
+    """
+    pass
--- Ergasia_1/test.py
+++ Ergasia_1/test.py
@@ -1,11 +1,17 @@
 import numpy as np
-from Ergasia_1.functions import vector_interp
+from functions import vector_interp
 
-np_load_old = np.load
-np.load = lambda *a,**k: np_load_old(*a, allow_pickle=True, **k)
+# np_load_old = np.load
+# np.load = lambda *a,**k: np_load_old(*a, allow_pickle=True, **k)
+# data = np.load('hw1.npy')
+# print(data)
+# np.load = np_load_old
 
-data = np.load('hw1.npy')
-print(data)
+p1 = [0, 10]
+p2 = [0, 0]
+V1 = [0, 0, 0]
+V2 = [2, 2, 2]
+coord = 0
+dim = 2
 
-
-np.load = np_load_old
+print(vector_interp(p1, p2, V1, V2, coord, dim))
