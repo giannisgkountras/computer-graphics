ff1a303ae22a681d5a5ca229c00443f0596c6344
giannisgkountras <gkountrasioannis@gmail.com>
2024-04-01 18:47:59 +0300

    Updated test.py for testing the functions seperately

--- Ergasia_1/test.py
+++ Ergasia_1/test.py
@@ -11,16 +11,17 @@ data = np.load("hw1.npy", allow_pickle=True).item()
 
 img_f = np.ones((512, 512, 3), dtype=float)
 img_g = np.ones((512, 512, 3), dtype=float)
+img = np.ones((512, 512, 3), dtype=float)
 
-# updated_img = f_shading(
-#     img,
-#     [
-#         [100, 40],
-#         [413, 500],
-#         [300, 40],
-#     ],
-#     [[1.0, 0.4, 0.2], [0.1, 0, 0.9], [0.0, 0.0, 1.0]],
-# )
+img = f_shading(
+    img,
+    [
+        [20, 20],
+        [250, 80],
+        [300, 400],
+    ],
+    [[1.0, 0.0, 0.0], [0, 1, 1], [1, 1.0, 0]],
+)
 
 
 # img_g = g_shading(
@@ -29,19 +30,24 @@ img_g = np.ones((512, 512, 3), dtype=float)
 #     [[1.0, 1.0, 0.0], [1.0, 1.0, 1.0], [0.9, 0.9, 0.9]],
 # )
 
-img_g = render_img(data["faces"], data["vertices"], data["vcolors"], data["depth"], "g")
-img_f = render_img(data["faces"], data["vertices"], data["vcolors"], data["depth"], "f")
+# img_g = render_img(data["faces"], data["vertices"], data["vcolors"], data["depth"], "g")
+# img_f = render_img(data["faces"], data["vertices"], data["vcolors"], data["depth"], "f")
 
 img_g *= 255
 img_f *= 255
+img *= 255
 
 img_g = img_g.astype(np.uint8)
 img_f = img_f.astype(np.uint8)
+img = img.astype(np.uint8)
 
 rgb_img_g = cv2.cvtColor(img_g, cv2.COLOR_BGR2RGB)
 rgb_img_f = cv2.cvtColor(img_f, cv2.COLOR_BGR2RGB)
+rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 cv2.imshow("Flat", rgb_img_g)
 cv2.imshow("Gouraud", rgb_img_f)
+cv2.imshow("g_shading", rgb_img)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
